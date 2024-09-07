dc877de67d3bbfd57501989fb89a2e8b1182e835
SPB <thespbgamer@outlook.com>
2024-09-07 20:03:31 +0100

    Small UI cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -51,21 +51,21 @@
 <div class="container grid place-content-center w-full">
 	<h1 class="my-2 text-4xl font-extrabold text-gray-900 md:text-5xl lg:text-6xl text-orange-500">Add Your Notes</h1>
 	<form on:submit|preventDefault={make_new_note}>
-		<h6 class="text-lg font-bold dark:text-white">New note text:</h6>
+		<h6 class="text-lg font-bold dark:text-white">Message:</h6>
 
 		<textarea
 			bind:value={new_note_text}
 			id="greet-input"
 			rows="10"
-			class="my-1 block p-2.5 w-full text-sm text-white bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-zinc-800 dark:border-zinc-600 dark:placeholder-zinc-500 dark:text-white dark:focus:ring-orange-500 dark:focus:border-orange-500"
+			class="my-1 block p-2.5 w-full text-sm text-white rounded-lg border bg-zinc-800 border-zinc-600 placeholder-zinc-500 focus:ring-orange-500 focus:border-orange-500"
 			placeholder="Write your note here..."
 			on:keydown={handleKeydown}
 		></textarea>
 
-		<h6 class="text-lg font-bold dark:text-white">Note Location:</h6>
+		<h6 class="text-lg font-bold text-white">Location:</h6>
 		<button
 			type="button"
-			class="text-xs mb-5 font-semibold text-gray-500 dark:text-zinc-300 cursor-pointer"
+			class="text-xs mb-5 font-semibold text-zinc-300 cursor-pointer"
 			on:click={() => {
 				navigator.clipboard.writeText(folderLocation);
 				currentNotification = "Location path was copied.";
@@ -81,7 +81,7 @@
 		<br />
 		<button
 			type="submit"
-			class="text-white w-full bg-orange-500 hover:bg-orange-400 focus:outline-none focus:ring-4 focus:ring-orange-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:focus:ring-orange-900"
+			class="text-white w-full bg-orange-500 hover:bg-orange-400 focus:outline-none focus:ring-4 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 focus:ring-orange-900"
 			>Submit</button
 		>
 	</form>
@@ -90,14 +90,14 @@
 		on:click|preventDefault={async () => {
 			folderLocation = await pickFolder(folderLocation);
 		}}
-		class="text-xs absolute right-5 bottom-5 text-white focus:outline-none focus:ring-4 focus:ring-zinc-300 rounded-full px-5 py-2.5 dark:bg-zinc-800 dark:hover:bg-zinc-600 dark:focus:ring-zinc-500 dark:border-zinc-600"
+		class="text-xs absolute right-5 bottom-5 text-white focus:outline-none focus:ring-4 rounded-full px-5 py-2.5 bg-zinc-900 hover:bg-zinc-600 focus:ring-zinc-500 border-zinc-600"
 		>Change Folder</button
 	><button
 		type="button"
 		on:click|preventDefault={async () => {
 			folderLocation = await resetFolderLocation();
 		}}
-		class="text-xs absolute left-5 bottom-5 text-white focus:outline-none focus:ring-4 focus:ring-zinc-300 rounded-full px-5 py-2.5 dark:bg-zinc-800 dark:hover:bg-zinc-600 dark:focus:ring-zinc-500 dark:border-zinc-600"
+		class="text-xs absolute left-5 bottom-5 text-white focus:outline-none focus:ring-4 rounded-full px-5 py-2.5 bg-zinc-900 hover:bg-zinc-600 focus:ring-zinc-500 border-zinc-600"
 		>Reset folder</button
 	>
 	{#if currentNotification}
